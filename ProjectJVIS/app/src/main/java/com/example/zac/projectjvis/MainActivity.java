f00da75164edda662f5b52965955182d748428f3
ZacAdams <zta104@gmail.com>
2016-01-19 16:26:38 -0600

    Removed TextView
    Make sure to go in and add button for new activity

--- ProjectJVIS/app/src/main/java/com/example/zac/projectjvis/MainActivity.java
+++ ProjectJVIS/app/src/main/java/com/example/zac/projectjvis/MainActivity.java
@@ -1,36 +1,62 @@
 package com.example.zac.projectjvis;
 
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.jcraft.jsch.*;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button button = (Button)findViewById(R.id.button);
-        final TextView text = (TextView)findViewById(R.id.text);
+        final Button button = (Button) findViewById(R.id.button);
+        final EditText username = (EditText) findViewById(R.id.editText);
+        final EditText password = (EditText) findViewById(R.id.editText2);
+        final EditText hostname = (EditText) findViewById(R.id.editText3);
+        final EditText entry = (EditText) findViewById(R.id.editText4);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String user = username.getText().toString();
+                final String pass = password.getText().toString();
+                final String host = hostname.getText().toString();
+                final String command = entry.getText().toString();
 
+
+                //System.out.print("first");
                 new AsyncTask<Integer, Void, Void>() {
                     @Override
                     protected Void doInBackground(Integer... params) {
                         try {
-                            String out = executeRemoteCommand("pi", "raspberry", "192.168.2.156", 22);
-                            System.out.println(out);
+                            executeRemoteCommand(user, pass, host, command, 22);
+                            System.out.println("executed");
 
                         } catch (Exception e) {
                             System.out.println(e);
@@ -40,8 +66,12 @@ public class MainActivity extends AppCompatActivity {
                 }.execute(1);
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-    public String executeRemoteCommand(String username, String password, String hostname, int port) throws JSchException {
+
+    public String executeRemoteCommand(String username, String password, String hostname, String command, int port) throws JSchException, IOException {
         JSch jsch = new JSch();
         Session session = jsch.getSession(username, hostname, port);
         session.setPassword(password);
@@ -56,15 +86,74 @@ public class MainActivity extends AppCompatActivity {
 
         //opening ssh channel
         ChannelExec channelssh = (ChannelExec) session.openChannel("exec");
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        channelssh.setOutputStream(baos);
+        //ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        //channelssh.setOutputStream(baos);
+
+        // Trying input stream
+        channelssh.setInputStream(null);
+        InputStream in = channelssh.getInputStream();
 
         //command to execute
-        channelssh.setCommand("ls");
+        channelssh.setCommand(command);
         channelssh.connect();
 
-        channelssh.disconnect();
+        byte[] tmp=new byte[1024];
+        while(true){
+            while(in.available()>0){
+                int i=in.read(tmp, 0, 1024);
+                if(i<0)break;
+                System.out.println(new String(tmp, 0, i));
+            }
+            if(channelssh.isClosed()){
+                System.out.println("exit-status: "+channelssh.getExitStatus());
+                break;
+            }
+            try{Thread.sleep(1000);}catch(Exception ee){}
+        }
 
-        return baos.toString();
+        channelssh.disconnect();
+        session.disconnect();
+
+        return in.toString();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.zac.projectjvis/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.zac.projectjvis/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
